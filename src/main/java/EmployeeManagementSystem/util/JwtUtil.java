ffcdc0e2bad5d0b57280bccef755fcfb3bf5a8ae
Rittik <rittikgoyal22@gmail.com>
2025-07-20 15:45:19 +0530

    edit secret key

--- src/main/java/EmployeeManagementSystem/util/JwtUtil.java
+++ src/main/java/EmployeeManagementSystem/util/JwtUtil.java
@@ -4,13 +4,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,18 +17,9 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    private String secretkey = "";
+    @Value("${jwt.secret}")
+    private String secretkey;
 
-    public JwtUtil() {
-
-        try {
-            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-            SecretKey sk = keyGen.generateKey();
-            secretkey = Base64.getEncoder().encodeToString(sk.getEncoded());
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public String generateToken(String username) {
         Map<String, Object> claims = new HashMap<>();
